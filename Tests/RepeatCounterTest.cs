6fc7b09540e6ffd089ef6243c454a0341db5fd0f
Adam Luchini <aluchini@gmail.com>
2016-07-08 08:57:07 -0700

    add passing second test. no matches for no matching words

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -5,12 +5,21 @@ namespace RepeatCounter.Objects
 {
   public class RepeatCounterTest
   {
+
     [Fact]
-    public void CountRepeats_InputWordFoundInInputSentenceIfExactMatch_1()
+    public void CountRepeats_InputWordFindsExactMatchInInputSentenceOnce_1()
     {
     string inputSentence = "Tia and Tamera";
     string inputWord = "Tia";
     Assert.Equal(1, RepeatCounter.CountRepeats(inputSentence, inputWord));
+    }
+
+    [Fact]
+    public void CountRepeats_InputWordDoesNotFindMatchInInputSentece_0()
+    {
+      string inputSentence = "Tia and Tamera";
+      string inputWord = "Urkle";
+      Assert.Equal(0, RepeatCounter.CountRepeats(inputSentence, inputWord));
     }
 
   }
